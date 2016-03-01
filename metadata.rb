7380d6f2c898f7a6778ecfcb554cc1c6fec82e1f
dmlb2000 <dmlb2000@gmail.com>
2016-02-29 22:05:46 -0800

    add some urls

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'dmlb2000@gmail.com'
 license 'all_rights'
 description 'Installs/Configures munge'
 long_description 'Installs/Configures munge'
+issues_url 'https://github.com/dmlb2000/munge-cookbook/issues'
+source_url 'https://github.com/dmlb2000/munge-cookbook'
 version '0.1.0'
 
 depends 'yum-epel'
