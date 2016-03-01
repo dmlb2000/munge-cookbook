4d93a28992a775a5b2dfd677bc8b59288cc10822
dmlb2000 <dmlb2000@gmail.com>
2016-02-29 23:42:45 -0800

    try to fix up the chefspec bits

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -8,6 +8,11 @@ require 'spec_helper'
 
 describe 'munge::default' do
   context 'When all attributes are default, on an unspecified platform' do
+    before do
+      Chef::EncryptedDataBagItem.stub(:load).with('munge', 'key').and_return(
+        mungekey: 'aRz24Kt4tAt2Fb5R1m3iqSNNpxFZKBsySWVMxM2phOc=',
+      )
+    end
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new
       runner.converge(described_recipe)
